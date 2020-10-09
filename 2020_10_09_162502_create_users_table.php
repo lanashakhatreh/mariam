e8d3745bcd5456c26b61093905d64cc561ae7d63
Lana Shakhatreh <46906592+lanashakhatreh@users.noreply.github.com>
2020-10-09 19:32:56 +0300

    Add files via upload

---
+++ 2020_10_09_162502_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('phone');
+            $table->string('address');
+            $table->integer('gender');
+            $table->integer('flat_id');
+            $table->string('contract_image');
+            $table->boolean('isDeleated');
+            $table->boolean('isFavorate');
+            $table->unsignedBigInteger ('role_id');
+            $table->foreign('role_id')->references('id')->on('role')->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
